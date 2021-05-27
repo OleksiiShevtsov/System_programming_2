faf6bdd99ef8b95e99508af97fed8efb4a2262c8
OleksiiShevtsov <70405962+OleksiiShevtsov@users.noreply.github.com>
2021-05-28 00:11:37 +0300

    home_work_8

--- 2_Jun/8_multithreading_criticalSection/home_work_8/home_work_8/main.cpp
+++ 2_Jun/8_multithreading_criticalSection/home_work_8/home_work_8/main.cpp
@@ -20,9 +20,11 @@ struct DataPrediction {
 class Gamble {
 
 public:
+
 	Gamble() {
 		srand(static_cast<unsigned int>(time(0)));
-		m_hiddenNumber = rand() % 100 + 1;
+		m_hiddenNumber = rand() % 20 + 1;
+		std::cout << "prediction: " << m_hiddenNumber << std::endl;
 	}
 
 	void MakePrediction(const char* userName, int prediction) {
@@ -32,8 +34,6 @@ public:
 
 		HANDLE thread = CreateThread(0, 0, ThreadStart, this, 0, 0);
 	}
-
-	DataPrediction m_dataPrediction;
 
 	void setPredictions(const char* userName, int prediction) {
 		m_predictions.insert(std::pair<const char*, int>(userName, prediction));
@@ -47,37 +47,51 @@ public:
 		int temp = 0;
 
 		for (auto iter = m_predictions.begin(); iter != m_predictions.end(); iter++) {
-			temp = m_hiddenNumber - iter->second;
+			if (iter == m_predictions.begin()) {
+				temp = abs(m_hiddenNumber - iter->second);
+				bestPrediction.first = iter->first;
+				bestPrediction.second = iter->second;
+				continue;
+			}
 			if (temp > abs(m_hiddenNumber - iter->second)) {
 				temp = abs(m_hiddenNumber - iter->second);
-				bestPrediction = *iter;
+				bestPrediction.first = iter->first;
+				bestPrediction.second = iter->second;
 			}
 		}
 		bestPredictions.insert(bestPrediction);
 		for (auto iter = m_predictions.begin(); iter != m_predictions.end(); iter++) {
-			temp = m_hiddenNumber - iter->second;
-			if (temp > abs(m_hiddenNumber - iter->second)) {
-				temp = abs(m_hiddenNumber - iter->second);
-				bestPrediction = *iter;
+			if (iter->second == bestPrediction.second) {
+				bestPredictions.insert(*iter);
 			}
 		}
+		for (auto iter = bestPredictions.begin(); iter != bestPredictions.end(); iter++) {
+			cout << "best prediction: " << iter->first << ": " << iter->second << endl;
+			cout << endl;
+		}
 	}
+
+	void allPredictions() {
+		for (auto iter = m_predictions.begin(); iter != m_predictions.end(); iter++) {
+			cout << "all prediction: " << iter->first << ": " << iter->second << std::endl;
+		}
+	}
+	
+	DataPrediction m_dataPrediction;
 
 private:
 
 	std::map<const char*, int> m_predictions;
-
 	int m_hiddenNumber;
 };
 
 DWORD WINAPI ThreadStart(void* param) {
-
 	EnterCriticalSection(&criticalSection);
 
 		Gamble* gamble = static_cast<Gamble*>(param);
-		cout << gamble->m_dataPrediction.predictionsValue << endl;
-		cout << gamble->m_dataPrediction.name << endl;
-		gamble->setPredictions(gamble->m_dataPrediction.name, gamble->m_dataPrediction.predictionsValue);
+		int value = gamble->m_dataPrediction.predictionsValue;
+		const char* name = gamble->m_dataPrediction.name;
+		gamble->setPredictions(name, value);
 
 	LeaveCriticalSection(&criticalSection);
 
@@ -90,9 +104,25 @@ int main() {
 
 	Gamble gamble;
 	
-	gamble.MakePrediction("Alex", 12);
+	gamble.MakePrediction("Alex1", 3);
+	Sleep(100);
+	gamble.MakePrediction("Alex2", 5);
+	Sleep(100);
+	gamble.MakePrediction("Alex3", 8);
+	Sleep(100);
+	gamble.MakePrediction("Alex5", 11);
+	Sleep(100);
+	gamble.MakePrediction("Alex6", 15);
+	Sleep(100);
+	gamble.MakePrediction("Alex7", 19);
+
+	Sleep(100);
+
+	gamble.allPredictions();
+	gamble.findAnAccuratePrediction();
 
 	Sleep(2000);
+
 	DeleteCriticalSection(&criticalSection);
 
 	return 0;
