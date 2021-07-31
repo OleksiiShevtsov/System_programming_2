3dc632795935eff6882aed6f18a6ca86991c0256
OleksiiShevtsov <70405962+OleksiiShevtsov@users.noreply.github.com>
2021-08-01 00:12:34 +0300

    task_24

--- 2_Jun/24/task_24/task_24/main.cpp
+++ 2_Jun/24/task_24/task_24/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <algorithm>
 
-size_t getLastWorldSizeFor(std::string& str) {
+size_t getLastWorldSizeFor(const std::string& str) {
 	for (int i = str.length(); i >= 0; i--) {
 		if (str[i] == ' ') {
 			return str.length() - i - 1;
@@ -16,20 +16,31 @@ size_t getLastWorldSize(const std::string& str) {
 }
 
 bool checkPolynom(const std::string& str) {
-	for (;;) {
-		str.find(' ');
-		str.in
+
+	std::string checkingStr(str);
+
+	checkingStr.erase(std::remove(checkingStr.begin(), checkingStr.end(), ' '), checkingStr.end());
+	std::transform(checkingStr.begin(), checkingStr.end(), checkingStr.begin(), tolower);
+	std::reverse(&checkingStr[0], &checkingStr[checkingStr.length()/2]);
+
+	std::string beginCheckingStr(checkingStr.begin(), (checkingStr.begin() + checkingStr.length() / 2));
+	std::string endCheckingStr((checkingStr.begin() + checkingStr.length() / 2), checkingStr.end());
+
+	if (beginCheckingStr.length() != endCheckingStr.length()) {
+		endCheckingStr.erase(endCheckingStr.begin(), (endCheckingStr.begin() + 1));
 	}
+	return beginCheckingStr == endCheckingStr;
 }
 
 int main() {
 
-	std::string str = "Alex World!";
+	std::string str1 = "Alex World!";
 
-	std::cout << getLastWorldSizeFor(str) << std::endl;
-	std::cout << getLastWorldSize(str) << std::endl;
+	std::cout << getLastWorldSizeFor(str1) << std::endl;
+	std::cout << getLastWorldSize(str1) << std::endl;
 
-	str = "Do geese see god";
+	std::string str2 = "Doge1e g o d";
+	std::cout << checkPolynom(str2) << std::endl;
 
 	return 0;
 }
